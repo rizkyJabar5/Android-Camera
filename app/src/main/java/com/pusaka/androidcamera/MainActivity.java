dae2e6b1aa68e0525999e571afb13e232f32c4e9
Rizky Khamet <rizkyjabar@it.student.pens.ac.id>
2020-04-23 20:10:35 +0700

    Firts Commit

---
+++ app/src/main/java/com/pusaka/androidcamera/MainActivity.java
@@ -0,0 +1,45 @@
+package com.pusaka.androidcamera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private Button btnCapture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCapture = findViewById(R.id.button_pressed);
+        imageView = findViewById(R.id.imageView);
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+    }
+
+    private void openCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            Bitmap bp = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bp);
+        }
+    }
+}
